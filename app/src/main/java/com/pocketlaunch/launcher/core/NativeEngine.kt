0e10f534050c294598dba0bdb4ce713fe3e800a4
AmethystWeb <fenrirfel0@gmail.com>
2026-07-20 22:16:47 +0800

    Update NativeEngine.kt

--- app/src/main/java/com/pocketlaunch/launcher/core/NativeEngine.kt
+++ app/src/main/java/com/pocketlaunch/launcher/core/NativeEngine.kt
@@ -1,1 +1,9 @@
+package com.pocketlaunch.launcher.core
+
+object NativeEngine {
+    init { System.loadLibrary("pocketlaunch") }
+    external fun initEngine(): Boolean
+    external fun toggleModule(moduleId: String, enabled: Boolean)
+    external fun isModuleEnabled(moduleId: String): Boolean
+}
 

f2ec26d3f935f20a4089a5c1f344c40f9bf0dc64
AmethystWeb <fenrirfel0@gmail.com>
2026-07-22 16:20:05 +0800

    Update ModuleCategory.kt

--- app/src/main/java/com/pocketlaunch/launcher/game/ModuleCategory.kt
+++ app/src/main/java/com/pocketlaunch/launcher/game/ModuleCategory.kt
@@ -1,1 +1,8 @@
+package com.pocketlaunch.launcher.game
 
+enum class ModuleCategory(val displayName: String) {
+    RENDER("Render"),
+    UTILITIES("Utilities"),
+    WORLD("World"),
+    MISC("Misc")
+}

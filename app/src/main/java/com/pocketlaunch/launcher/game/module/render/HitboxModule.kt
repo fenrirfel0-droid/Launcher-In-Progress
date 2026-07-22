ae34752cc20f0ad48c3ed288a500a5b48001b686
AmethystWeb <fenrirfel0@gmail.com>
2026-07-22 16:45:22 +0800

    Update HitboxModule.kt

--- app/src/main/java/com/pocketlaunch/launcher/game/module/render/HitboxModule.kt
+++ app/src/main/java/com/pocketlaunch/launcher/game/module/render/HitboxModule.kt
@@ -1,1 +1,24 @@
+package com.pocketlaunch.launcher.game.module.render
 
+import android.content.Context
+import android.util.Log
+import com.pocketlaunch.launcher.game.Module
+import com.pocketlaunch.launcher.game.ModuleCategory
+
+class HitboxModule : Module(
+    id = "hitbox",
+    name = "Hitbox Display",
+    description = "Renders bounding box visual outlines around entities.",
+    category = ModuleCategory.RENDER
+) {
+    var showEyeLine: Boolean = true
+    var showDirectionVector: Boolean = true
+
+    override fun onEnable(context: Context) {
+        Log.d("HitboxModule", "Hitbox visualization enabled")
+    }
+
+    override fun onDisable(context: Context) {
+        Log.d("HitboxModule", "Hitbox visualization disabled")
+    }
+}

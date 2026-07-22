06e04efef2676554ef5534682b7eac9dca34decc
AmethystWeb <fenrirfel0@gmail.com>
2026-07-22 16:20:50 +0800

    Update Module.kt

--- app/src/main/java/com/pocketlaunch/launcher/game/Module.kt
+++ app/src/main/java/com/pocketlaunch/launcher/game/Module.kt
@@ -1,1 +1,27 @@
+package com.pocketlaunch.launcher.game
 
+import android.content.Context
+
+abstract class Module(
+    val id: String,
+    val name: String,
+    val description: String,
+    val category: ModuleCategory
+) {
+    var isEnabled: Boolean = false
+        private set
+
+    open fun onEnable(context: Context) {}
+    open fun onDisable(context: Context) {}
+    open fun onTick(context: Context) {}
+
+    fun toggle(context: Context): Boolean {
+        isEnabled = !isEnabled
+        if (isEnabled) {
+            onEnable(context)
+        } else {
+            onDisable(context)
+        }
+        return isEnabled
+    }
+}

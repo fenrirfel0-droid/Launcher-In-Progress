0956ea38b21c782f45b55502548c02fcc3b94fb1
AmethystWeb <fenrirfel0@gmail.com>
2026-07-22 16:49:29 +0800

    Update CrystalOptimizerModule.kt

--- app/src/main/java/com/pocketlaunch/launcher/game/module/render/CrystalOptimizerModule.kt
+++ app/src/main/java/com/pocketlaunch/launcher/game/module/render/CrystalOptimizerModule.kt
@@ -1,1 +1,15 @@
+package com.pocketlaunch.launcher.game.module.render
 
+import android.content.Context
+import com.pocketlaunch.launcher.game.Module
+import com.pocketlaunch.launcher.game.ModuleCategory
+
+class CrystalOptimizerModule : Module(
+    id = "crystal_optimizer",
+    name = "Crystal Optimization",
+    description = "Optimizes end crystal explosion particles and animation ticks.",
+    category = ModuleCategory.RENDER
+) {
+    var reduceExplosionParticles: Boolean = true
+    var fastCrystalBreakAnim: Boolean = true
+}

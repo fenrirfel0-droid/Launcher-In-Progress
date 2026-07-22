796ad6c1fda74f93ad5b44e1bdc1eb3936d9b2d2
AmethystWeb <fenrirfel0@gmail.com>
2026-07-22 16:52:53 +0800

    Update ZoomModule.kt

--- app/src/main/java/com/pocketlaunch/launcher/game/module/utilities/ZoomModule.kt
+++ app/src/main/java/com/pocketlaunch/launcher/game/module/utilities/ZoomModule.kt
@@ -1,1 +1,20 @@
+package com.pocketlaunch.launcher.game.module.utilities
 
+import android.content.Context
+import com.pocketlaunch.launcher.game.Module
+import com.pocketlaunch.launcher.game.ModuleCategory
+
+class ZoomModule : Module(
+    id = "zoom",
+    name = "Zoom",
+    description = "Magnifies field of view for scouting and aiming.",
+    category = ModuleCategory.UTILITIES
+) {
+    var isZooming: Boolean = false
+    var zoomFovTarget: Int = 30
+
+    fun toggleZoom(): Int {
+        isZooming = !isZooming
+        return if (isZooming) zoomFovTarget else 70
+    }
+}
